9c721302a59a851d85d532246c92b1ef5d5ea803
Michael Dekker <git@michaeldekker.nl>
2017-02-05 07:57:28 +0100

    Update blocknewproducts.php

--- blocknewproducts.php
+++ blocknewproducts.php
@@ -24,7 +24,7 @@
 *  International Registered Trademark & Property of PrestaShop SA
 */
 
-if (!defined('_PS_VERSION_'))
+if (!defined('_TB_VERSION_'))
 	exit;
 
 class BlockNewProducts extends Module
@@ -35,8 +35,8 @@ class BlockNewProducts extends Module
 	{
 		$this->name = 'blocknewproducts';
 		$this->tab = 'front_office_features';
-		$this->version = '1.10.1';
-		$this->author = 'PrestaShop';
+		$this->version = '2.0.0';
+		$this->author = 'thirty bees';
 		$this->need_instance = 0;
 
 		$this->bootstrap = true;
