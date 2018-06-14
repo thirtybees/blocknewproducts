083521462c49a1b081ea88def3be2bbeaaf6ec02
Markus Hitter <mah@jump-ing.de>
2018-06-14 16:39:58 +0200

    Make release 2.1.3.

--- blocknewproducts.php
+++ blocknewproducts.php
@@ -53,7 +53,7 @@ class BlockNewProducts extends Module
     {
         $this->name = 'blocknewproducts';
         $this->tab = 'front_office_features';
-        $this->version = '2.1.2';
+        $this->version = '2.1.3';
         $this->author = 'thirty bees';
         $this->need_instance = 0;
 
