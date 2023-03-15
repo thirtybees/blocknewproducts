a2e5821c9e6cdb4efd4cc0ee6a993440648ba9e3
Petr Hučík <petr@getdatakick.com>
2023-03-15 09:15:44 +0100

    Release blocknewproducts v2.3.0

--- blocknewproducts.php
+++ blocknewproducts.php
@@ -48,7 +48,7 @@ class BlockNewProducts extends Module
     {
         $this->name = 'blocknewproducts';
         $this->tab = 'front_office_features';
-        $this->version = '2.2.3';
+        $this->version = '2.3.0';
         $this->author = 'thirty bees';
         $this->need_instance = 0;
 
